31613ececc63b3012f701e5b19b66603e08cb9f4
Rafael Braga <rafaelbra149@gmail.com>
2025-09-26 10:07:38 -0400

    Adding call to action button

--- src/components/shared/NavItem.tsx
+++ src/components/shared/NavItem.tsx
@@ -5,7 +5,7 @@ interface NavItemProps {
     text: string;
 }
 
-export default function NavItem({href, text = ""} : NavItemProps) {
+export default function NavItem({href, text } : NavItemProps) {
     return (
         <li>
             <a href={href} className="duration-300 font-medium ease-linear hover:text-primary py-3">
