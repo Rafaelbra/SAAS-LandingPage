b643ef6307befb22a45bc76d89eabb99dd6f5e9e
Rafael Braga <rafaelbra149@gmail.com>
2025-09-26 16:23:16 -0400

    Introducing Glow effect to Hero Section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,9 +1,19 @@
+import Container from "../shared/Container";
 
 
 export default function Hero() {
     return (
-        <section>
-            
+        <section className="relative pt-32 lg:pt-36">
+            <Container className="flex flex-col lg:flex-row gap-10 lg:gap-12">
+                <div className="absolute w-full lg:w-1/2 inset-y-0 lg:right-0">
+                    <span className="absolute -left-6 md:left-4 top-24 lg:top-28 w-24 h-24 rotate-90 skew-x-12 rounded-3xl bg-gradient-to-r from-blue-600 to-violet-600 blur-xl opacity-60 lg:opacity-95 lg:block hidden">
+
+                    </span>
+                    <span className="absolute right-4 bottom-12 w-24 h-24 rounded-3xl bg-primary blur-xl opacity-80">
+                        
+                    </span>
+                </div>
+            </Container>
         </section>
     )
 }
