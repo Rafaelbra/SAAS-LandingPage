6f990591e7eb27ecacc3ca94e11545a8d6f3b8f0
Rafael Braga <rafaelbra149@gmail.com>
2025-09-27 16:27:18 -0400

    Finishing styling for About Us Section

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -1,4 +1,7 @@
+import { Info } from "../cards/Info"
 import { Container } from "../shared/Container"
+import Paragraph from "../shared/Paragraph"
+import { Title } from "../shared/Title"
 
 export const AboutUs = () => {
     return (
@@ -7,10 +10,59 @@ export const AboutUs = () => {
                 <div className="w-full md:w-5/12 lg:w-1/2">
                     <div className="w-full h-80 sm:h-96 relative">
                         <img
-                            src="https://images.pexels.com/photos/8728381/pexels-photo-8728381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                            className="w-full h-full object-cover rounded-3xl shadow-lg relative z-10"
-                            alt="About Our Mission"
+                        src="https://images.pexels.com/photos/8728381/pexels-photo-8728381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        className="w-full h-full object-cover rounded-3xl shadow-lg relative z-10"
+                        alt="About Our Mission"
                         />
+                    </div>
+                </div>
+
+                <div className="w-full md:w-7/12 lg:w-1/2 flex flex-col">
+                    <Title> About Our AI Solution</Title>
+                    <Paragraph>
+                        At LuminAI, we empower businesses to unlock their full potential with next-generation artificial intelligence. By combining cutting-edge research with practical, real world innovation, we deliver intelligent solutions that adapt to your unique challenges. Whether you’re optimizing workflows, enhancing customer experiences, or driving data powered decisions, LuminAI helps you lead with confidence in the AI era.
+                    </Paragraph>
+                    <div className="pt-8 grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-3xl">
+                        <Info
+                         title="Mission"
+                         description="Our mission is to use AI for the good of humanity and the planet."
+                        >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="30"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-4 h-4 sm:w-5 sm:h-5"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+                        />
+                        </svg>
+                        </Info>
+                        <Info
+                            title="Vision"
+                            description="Our vision is to drive business innovation and growth."
+                            >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-4 h-4 sm:w-5 sm:h-5"
+                            >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+                            />
+                            </svg>
+                        </Info>
                     </div>
                 </div>
             </Container>
