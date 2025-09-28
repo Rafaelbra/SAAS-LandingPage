cadfc0a2d3cf604dd478c33fae92d54e1a264c6c
Rafael Braga <rafaelbra149@gmail.com>
2025-09-28 18:11:29 -0400

    Finishing CTA section

--- src/components/sections/CallToAction.tsx
+++ src/components/sections/CallToAction.tsx
@@ -1,3 +1,4 @@
+import Button from "../shared/Button"
 import { Container } from "../shared/Container"
 import Paragraph from "../shared/Paragraph"
 
@@ -6,15 +7,21 @@ export const CTA = () => {
         <section className="pb-20 relative">
             <Container>
                 <div className="relative rounded-2xl overflow-hidden">
-                    <div>
-                        <h1>
-                            Quick Start your <span> own AI</span> Business
+                    <div className="relative z-10 mx-auto text-center max-w-xl md:max-w-2xl py-8 md:py-10 px-6 md:px-8">
+                        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-heading-1">
+                            Quick Start your 
+                            <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-violet-600"> own AI</span> Business
                         </h1>
-                        <Paragraph>
+                        <Paragraph className="pt-10">
                             Leverage our AI-powered platform to revolutionize your digital
                             marketing efforts. Get data-driven insights and automation at your
                             fingertips.
                         </Paragraph>
+                        <div className="mx-auto max-w-md sm:max-w-xl pt-10 dark:text-white">
+                            <Button>
+                                Get In Touch
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </Container>
